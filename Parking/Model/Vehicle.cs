d6dcf5088d129e202c7ff47e82e58c12493c8bb3
Patryk Janicki <kontakt.janicki@gmail.com>
2019-01-14 16:37:33 +0100

    Dodanie klasy reprezentującej pojazd

---
+++ Parking/Model/Vehicle.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Parking.Model {
+    class Vehicle {
+        public enum VehicleType { Car, Motorcycle }
+
+        private string id;
+        private VehicleType type;
+
+        public Vehicle(string id, VehicleType type) {
+            this.id = id;
+            this.type = type;
+        }
+    }
+}
