10a36b2bc5f8d1fcd28f8f0dd049e9edc04bc686
Patryk Janicki <kontakt.janicki@gmail.com>
2019-01-14 20:22:08 +0100

    Dodanie klasy DataContext do projektu Parking.Services

---
+++ Parking.Services/DataContext.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Parking.Model;
+
+namespace Parking.Services {
+    class DataContext {
+        public ObservableCollection<Vehicle> vehicles;
+        public ObservableCollection<Visit> visits;
+        public ObservableCollection<Vehicle> payments;
+    }
+}
