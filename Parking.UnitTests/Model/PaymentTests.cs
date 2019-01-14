83fe4a65ec10c34d01852a69b4ac9a4f67231ae8
Patryk Janicki <kontakt.janicki@gmail.com>
2019-01-14 19:36:19 +0100

    Dodanie testów jednostkowych dla klasy Payment

---
+++ Parking.UnitTests/Model/PaymentTests.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Parking.Model;
+
+namespace Parking.UnitTests.Model {
+    [TestClass]
+    public class PaymentTests {
+        [TestMethod]
+        public void CorrectlyInitialize() {
+            Vehicle vehicle = new Vehicle("yyy", Vehicle.VehicleType.Motorcycle);
+            DateTimeOffset datetime = DateTimeOffset.Now.AddHours(-2);
+            Visit visit = new Visit(1, vehicle, datetime);
+            Payment payment = new Payment(1, visit, 100d);
+        }
+    }
+}
