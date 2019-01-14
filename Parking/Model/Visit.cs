697f4d584cf1d4b19c27ad35e35a6af10a647f87
Patryk Janicki <kontakt.janicki@gmail.com>
2019-01-14 16:43:42 +0100

    Dodanie modelu wizyty

---
+++ Parking/Model/Visit.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Parking.Model {
+    class Visit {
+        private static int index = 0;
+        private int id;
+        private Vehicle vehicle;
+        private DateTimeOffset startDate;
+        private DateTimeOffset endDate;
+
+        public Visit(Vehicle vehicle, DateTimeOffset startDate, DateTimeOffset endDate = default(DateTimeOffset)) {
+            id = index++;
+            this.vehicle = vehicle;
+            this.startDate = startDate;
+            this.endDate = endDate;
+        }
+    }
+}
