6dce31d11a3bf58c6da89b02f2a1a20936bcc284
Patryk Janicki <kontakt.janicki@gmail.com>
2019-01-14 16:46:05 +0100

    Dodanie klasy płatności

---
+++ Parking/Model/Payment.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Parking.Model {
+    class Payment {
+        private Visit visit;
+        private double value;
+        private DateTimeOffset date;
+
+        public Payment(Visit visit, double value) {
+            this.visit = visit;
+            this.value = value;
+            date = DateTimeOffset.Now;
+        }
+    }
+}
