bf99cc4ab679919f833c2b425de0b91b0c1453f9
Patryk Janicki <kontakt.janicki@gmail.com>
2019-01-14 20:32:19 +0100

    Inicjalizacja klasy DataRepository

---
+++ Parking.Services/DataRepository.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Parking.Services {
+    public partial class DataRepository {
+        private DataContext dataContext = new DataContext();
+
+        public DataRepository() {
+
+        }
+    }
+}
