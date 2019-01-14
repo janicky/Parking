52262bb7767e2037e70a1eb4dcc6a08c7483edbd
Patryk Janicki <kontakt.janicki@gmail.com>
2019-01-14 17:59:17 +0100

    Dodanie pola id do klasy Payment

--- Parking/Model/Payment.cs
+++ Parking/Model/Payment.cs
@@ -6,14 +6,21 @@ using System.Threading.Tasks;
 
 namespace Parking.Model {
     class Payment {
+        public static int index = 0;
+        private int id;
         private Visit visit;
         private double value;
         private DateTimeOffset date;
 
         public Payment(Visit visit, double value) {
+            id = index++;
             this.visit = visit;
             this.value = value;
             date = DateTimeOffset.Now;
+        }
+
+        public int GetId() {
+            return id;
         }
 
         public Visit GetVisit() {
