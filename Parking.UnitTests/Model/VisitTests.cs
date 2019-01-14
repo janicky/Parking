76c60abcf15307274dbb29973b67312e3e247d08
Patryk Janicki <kontakt.janicki@gmail.com>
2019-01-14 19:00:50 +0100

    Inicjalizacja testów jednostkowych klasy Visit

---
+++ Parking.UnitTests/Model/VisitTests.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Parking.Model;
+
+namespace Parking.UnitTests.Model {
+    [TestClass]
+    public class VisitTests {
+
+        private Vehicle vehicle;
+        private Visit visit;
+
+        [TestInitialize]
+        public void TestInitialize() {
+            vehicle = new Vehicle("xxx", Vehicle.VehicleType.Car);
+            visit = new Visit(1, vehicle, DateTimeOffset.Now);
+        }
+
+        [TestMethod]
+        public void IsNotFinished() {
+            Assert.AreEqual(false, visit.IsFinished());
+        }
+    }
+}
