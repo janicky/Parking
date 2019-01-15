3374cb4d078c5f89b7699eb84c455ba1cf7212cd
Patryk Janicki <kontakt.janicki@gmail.com>
2019-01-15 15:36:28 +0100

    Inicjalizacja repozytorium klasy Payment

---
+++ Parking.Services/DataRepository/PaymentsRepository.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Parking.Model;
+
+namespace Parking.Servicesy {
+    public partial class DataRepository {
+        
+        public List<Payment> GetAllPayments() {
+            return new List<Payment>();
+        }
+
+        public Payment GetPayment(int id) {
+            return null;
+        }
+
+        public void CreatePayment(Payment payment) {
+
+        }
+
+        public void UpdatePayment(Payment payment) {
+
+        }
+
+        public void DeletePayment(int id) {
+
+        }
+    }
+}
