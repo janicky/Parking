7b79ef9b153bc68fcb7233b7917c011f0b4a6020
Patryk Janicki <kontakt.janicki@gmail.com>
2019-01-15 22:25:37 +0100

    Utworzenie sekcji Details dla klasy Vehicle

--- Parking/ViewModel/Commands/SelectVehicleCommand.cs
+++ Parking/ViewModel/Commands/SelectVehicleCommand.cs
@@ -3,11 +3,15 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
 using System.Windows.Input;
 
 namespace Parking.ViewModel.Commands {
     class SelectVehicleCommand : ICommand {
         public event EventHandler CanExecuteChanged;
+
+        public SelectVehicleCommand(VehiclesViewModel vm) {
+        }
 
         public bool CanExecute(object parameter) {
             throw new NotImplementedException();
