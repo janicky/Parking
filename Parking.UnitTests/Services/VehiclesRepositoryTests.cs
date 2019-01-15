d4b6215313cc3b6ac5e71eab259f00d65b0568bc
Patryk Janicki <kontakt.janicki@gmail.com>
2019-01-15 11:31:48 +0100

    Dodanie wyjątku w metodzie GetVehicle

--- Parking.UnitTests/Services/VehiclesRepositoryTests.cs
+++ Parking.UnitTests/Services/VehiclesRepositoryTests.cs
@@ -1,13 +1,13 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
-using Parking.Services;
-using Parking.Model;
-using System.Collections.Generic;
 using SQLitePCL;
+using Parking.Model;
+using Parking.Services;
+using System.Collections.Generic;
 
 namespace Parking.UnitTests.Services {
     [TestClass]
-    public class DataRepositoryTests {
+    public class VehiclesRepositoryTests {
         private DataRepository dr = new DataRepository();
 
         [TestInitialize]
@@ -16,10 +16,20 @@ namespace Parking.UnitTests.Services {
         }
 
         [TestMethod]
-        public void GetVehicles() {
+        public void CorrectlyReturnsAllVehicles() {
             List<Vehicle> vehicles = new List<Vehicle>(dr.GetAllVehicles());
             Assert.AreEqual(1, vehicles.Count);
             Assert.AreEqual("XXX001", vehicles[0].Id);
+            Assert.AreEqual(1, vehicles[0].VehicleType);
+        }
+
+        [TestMethod]
+        public void CorrectlyReturnsSpecifiedVehicle() {
+            Vehicle vehicle = dr.GetVehicle("XXX001");
+            Assert.AreEqual("XXX001", vehicle.Id);
+
+            Vehicle incorrect_vehicle = dr.GetVehicle("XXX002");
+            Assert.AreEqual("XXX001", incorrect_vehicle.Id);
         }
     }
 }
