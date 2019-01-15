05f71e40a4b263291fc1dc5c7eb3fc620eb6e707
Patryk Janicki <kontakt.janicki@gmail.com>
2019-01-15 19:45:07 +0100

    Dodanie modelu Vehicles

---
+++ Parking/Model/Vehicles.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Parking.Services;
+
+namespace Parking.Model {
+
+    class Vehicles {
+
+        DataRepository dataRepository = new DataRepository();
+
+        public IEnumerable<Vehicle> All() {
+            return dataRepository.GetAllVehicles();
+        }
+
+        public Vehicle GetVehicle(string id) {
+            return dataRepository.GetVehicle(id);
+        }
+
+        public void CreateVehicle(Vehicle vehicle) {
+            dataRepository.CreateVehicle(vehicle);
+        }
+
+        public void UpdateVehicle(Vehicle vehicle) {
+            dataRepository.UpdateVehicle(vehicle);
+        }
+
+        public void DeleteVehicle(string id) {
+            dataRepository.DeleteVehicle(id);
+        }
+    }
+}
