35dbdf18366a26c9daea194947d1128ccd42ce3e
Patryk Janicki <kontakt.janicki@gmail.com>
2019-01-15 20:37:07 +0100

    Dodanie modelu Visits

---
+++ Parking/Model/Visits.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using Parking.Model;
+using Parking.Services;
+
+namespace Parking.Model {
+    class Visits {
+        DataRepository dataRepository = new DataRepository();
+
+        public IEnumerable<Visit> All() {
+            return dataRepository.GetAllVisits();
+        }
+
+        public Visit Get(int id) {
+            return dataRepository.GetVisit(id);
+        }
+
+        public void Create(Visit visit) {
+            dataRepository.CreateVisit(visit);
+        }
+
+        public void Update(Visit visit) {
+            dataRepository.UpdateVisit(visit);
+        }
+
+        public void Delete(int id) {
+            dataRepository.DeleteVisit(id);
+        }
+    }
+}
