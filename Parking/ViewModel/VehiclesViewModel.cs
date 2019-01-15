6c5835642e358a4976ecb97abf25dcdf825466b6
Patryk Janicki <kontakt.janicki@gmail.com>
2019-01-15 19:15:43 +0100

    Inicjalizacja klasy VehiclesViewModel

--- Parking/ViewModel/VehiclesViewModel.cs
+++ Parking/ViewModel/VehiclesViewModel.cs
@@ -1,12 +1,35 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Parking.Model;
 
 namespace Parking.ViewModel {
+    
     class VehiclesViewModel : INotifyPropertyChanged {
         public event PropertyChangedEventHandler PropertyChanged;
+        private ObservableCollection<Vehicle> vehicles;
+
+        public VehiclesViewModel() {
+            var newVehicles = new ObservableCollection<Vehicle>();
+            newVehicles.Add(new Vehicle("XXX0001", Vehicle.Type.Car));
+            VehiclesCollection = newVehicles;
+        }
+
+        public ObservableCollection<Vehicle> VehiclesCollection {
+            get => vehicles;
+            set {
+                if (vehicles == value) return;
+                vehicles = value;
+                OnPropertyChanged("VehiclesCollection");
+            }
+        }
+
+        protected void OnPropertyChanged(string name) {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("VehiclesCollection"));
+        }
     }
 }
