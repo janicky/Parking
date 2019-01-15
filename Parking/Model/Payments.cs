961f50a4b25338de69382bd8a0539c630015256e
Patryk Janicki <kontakt.janicki@gmail.com>
2019-01-15 20:39:33 +0100

    Dodanie modelu Payments

---
+++ Parking/Model/Payments.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using Parking.Services;
+
+namespace Parking.Model {
+    class Payments {
+        DataRepository dataRepository = new DataRepository();
+
+        public IEnumerable<Payment> All() {
+            return dataRepository.GetAllPayments();
+        }
+
+        public Payment Get(int id) {
+            return dataRepository.GetPayment(id);
+        }
+
+        public void Create(Payment payment) {
+            dataRepository.CreatePayment(payment);
+        }
+
+        public void Update(Payment payment) {
+            dataRepository.UpdatePayment(payment);
+        }
+
+        public void Delete(int id) {
+            dataRepository.DeletePayment(id);
+        }
+    }
+}
