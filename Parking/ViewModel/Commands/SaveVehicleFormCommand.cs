b1ecc784ce6f98bfb675b6ecd9cd460c53b3ca60
Patryk Janicki <kontakt.janicki@gmail.com>
2019-01-16 21:28:16 +0100

    Dodanie klasy SaveVehicleFormCommand

---
+++ Parking/ViewModel/Commands/SaveVehicleFormCommand.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace Parking.ViewModel.Commands {
+    class SaveVehicleFormCommand : ICommand {
+        public event EventHandler CanExecuteChanged {
+            add => CommandManager.RequerySuggested += value;
+            remove => CommandManager.RequerySuggested -= value;
+        }
+
+        private VehicleFormViewModel vm;
+        public SaveVehicleFormCommand(VehicleFormViewModel vm) {
+            this.vm = vm;
+        }
+
+        public bool CanExecute(object parameter) {
+            return true;
+        }
+
+        public void Execute(object parameter) {
+
+        }
+    }
+}
