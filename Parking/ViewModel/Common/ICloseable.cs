13b9af8ec43a254c1e0d92a0f11bd4035d9ff303
Patryk Janicki <kontakt.janicki@gmail.com>
2019-01-16 22:07:44 +0100

    Dodanie interfejsu ICloseable do projektu

---
+++ Parking/ViewModel/Common/ICloseable.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Parking.ViewModel.Common {
+    interface ICloseable {
+        event EventHandler<EventArgs> RequestClose;
+    }
+}
