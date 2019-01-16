d6102c8b3f232197ac5dd377ce8e4102a14dfe54
Patryk Janicki <kontakt.janicki@gmail.com>
2019-01-16 22:39:19 +0100

    Dodanie klasy EditVehicleCommand

---
+++ Parking/ViewModel/Commands/EditVehicleCommand.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+
+namespace Parking.ViewModel.Commands {
+    class EditVehicleCommand : ICommand {
+        public event EventHandler CanExecuteChanged {
+            add => CommandManager.RequerySuggested += value;
+            remove => CommandManager.RequerySuggested -= value;
+        }
+
+        private VehiclesViewModel vm;
+        public EditVehicleCommand(VehiclesViewModel vm) {
+            this.vm = vm;
+        }
+
+        public bool CanExecute(object parameter) {
+            return true;
+        }
+
+        public void Execute(object parameter) {
+
+        }
+    }
+}
