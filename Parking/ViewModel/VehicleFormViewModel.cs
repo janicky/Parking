0ab0c1e41b7d2651a0c5a8b68b89080b12e7df56
Patryk Janicki <kontakt.janicki@gmail.com>
2019-01-16 21:35:04 +0100

    Podpięcie kontrolek w formularz pojazdu

--- Parking/ViewModel/VehicleFormViewModel.cs
+++ Parking/ViewModel/VehicleFormViewModel.cs
@@ -8,11 +8,22 @@ namespace Parking.ViewModel {
     class VehicleFormViewModel : ViewModel {
 
         private string _title = "Formularz pojazdu";
+        private string _id;
+        private int _vehicleType = 0;
 
         public string Title {
             get => _title;
             set { _title = value; OnPropertyChanged("Title"); }
         }
 
+        public string Id {
+            get => _id;
+            set { _id = value; OnPropertyChanged("Id"); }
+        }
+
+        public int VehicleType {
+            get => _vehicleType;
+            set { _vehicleType = value; OnPropertyChanged("VehicleType"); }
+        }
     }
 }
