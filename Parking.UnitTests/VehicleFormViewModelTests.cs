fb89d61a07fe9fc95f8e138cefead4e5cb6980c9
Patryk Janicki <kontakt.janicki@gmail.com>
2019-01-17 15:27:35 +0100

    Inicjalizacja testu klasy VehicleFormViewModel

---
+++ Parking.UnitTests/VehicleFormViewModelTests.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Parking.ViewModel;
+using Parking.Model;
+
+namespace Parking.UnitTests {
+    [TestClass]
+    public class VehicleFormViewModelTests {
+
+        private VehicleFormViewModel vfvm;
+
+        public void SaveVehicleForm(string plate, int vehicleType) {
+            // todo
+        }
+
+        [TestInitialize]
+        public void TestInitialize() {
+            vfvm = new VehicleFormViewModel(SaveVehicleForm);
+        }
+
+        [TestMethod]
+        public void CorrectlyFillProperties() {
+            Vehicle vehicle = new Vehicle(-1, "FFF-001", Vehicle.Type.Car);
+            VehicleFormViewModel v = new VehicleFormViewModel(SaveVehicleForm, null, null, vehicle);
+            Assert.AreEqual(vehicle.Plate, v.Plate);
+            Assert.AreEqual(vehicle.VehicleType, v.VehicleType + 1);
+        }
+    }
+}
