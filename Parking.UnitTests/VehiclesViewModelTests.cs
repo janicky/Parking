60b3e38d960b21f0160749114674c6ee22abd316
Patryk Janicki <kontakt.janicki@gmail.com>
2019-01-17 12:05:29 +0100

    Dodanie testu zaznaczonego pojazdu

--- Parking.UnitTests/VehiclesViewModelTests.cs
+++ Parking.UnitTests/VehiclesViewModelTests.cs
@@ -1,12 +1,14 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Parking.ViewModel;
+using Parking.Model;
 
 namespace Parking.UnitTests {
     [TestClass]
     public class VehiclesViewModelTests {
 
         private VehiclesViewModel vvm;
+        private Visits Visits = new Visits();
 
         [TestInitialize]
         public void TestInitialize() {
@@ -20,6 +22,14 @@ namespace Parking.UnitTests {
             Assert.AreEqual(null, vvm.SelectedVehicle);
             Assert.AreEqual(null, vvm.AddVehicleWindow);
             Assert.AreEqual(null, vvm.EditVehicleWindow);
+        }
+
+        [TestMethod]
+        public void CorrectVisitForSelectedVehicle() {
+            Assert.AreEqual(null, vvm.GetVisitForSelectedVehicle());
+            vvm.SelectedVehicle = vvm.VehiclesCollection[0];
+            Visit visit = Visits.GetForVehicle(vvm.SelectedVehicle.Id);
+            Assert.AreEqual(visit.Id, vvm.GetVisitForSelectedVehicle().Id);
         }
     }
 }
